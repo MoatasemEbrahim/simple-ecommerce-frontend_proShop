3aaf0a1f9209f907ae50c1eb1692325e6e9f1c0a
moatasem.ebrahim <moatasem.ebrahim@fixedmea.com>
2020-11-14 13:31:55 +0200

    React,Typescript and Esling config

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Header
+      </header>
+      <main>
+        proShop
+      </main>
+      <footer>
+        footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
